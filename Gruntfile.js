953aa56d43975f4e468be3bf7902c9ad7ba9348d
Samuel Samtleben <samuel.samtleben@ausdatteln.de>
2015-12-13 11:43:10 +0100

    asmcrypto.js and es6-promise no longer directly included

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,20 +7,8 @@ module.exports = function(grunt) {
       asmcrypto: {
         options: {
           repository: 'https://github.com/vibornoff/asmcrypto.js.git',
-          branch: 'v0.0.10',
-          cwd: 'lib'
-        }
-      }
-    },
-    auto_install: {
-      local: {
-        options: {
-          recursive: false
-        }
-      },
-      asmcrypto: {
-        options: {
-          cwd: 'lib/asmcrypto.js'
+          branch: 'master',
+          cwd: 'repos'
         }
       }
     },
@@ -33,27 +21,22 @@ module.exports = function(grunt) {
                   + '(c) 2015 Samuel Samtleben \n'
                   + 'License: MIT \n'
                   + '*/\n' 
-                  + '(function(exports, global, undefined) { \n'
-                  + '"use strict";\n',
-          footer: '\n\n typeof define === "function" && define.amd '
-                    + '? define([], function() {return exports}) '
-                    + ': typeof module === "object" && module.exports '
-                        + '? module.exports = exports '
-                        + ': global.webCrypto = exports;'
-                    + '\n return exports;\n'
-                    + '})({}, function() {return this}());'
+                  + '(function(root, factory) { \n'
+                    + '\tif (typeof define === "function" && define.amd) {\n'
+                      + '\t\tdefine(["asmCrypto"], factory);\n'
+                    + '\t} else if (typeof exports === "object") {\n'
+                      + '\t\tmodule.exports = factory(require("asmCrypto")); \n'
+                    + '\t} else { \n'
+                      + '\t\troot.webCrypto = factory(root.asmCrypto);\n'
+                    + '\t}\n'
+                  + '}(this, function (asmCrypto) {\n'
+                    + '\tvar exports = {};\n',
+          footer: '\nreturn exports;\n' 
+                  + '}));'
         },
         src: [
           'src/**/*.js'],
-        dest: 'dist/web-crypto-src.js'
-      },
-      default: {
-        files: {
-          'dist/web-crypto.js': ['lib/asmcrypto.js/asmcrypto.js', 'dist/web-crypto-src.js'],
-          'dist/web-crypto.min.js': ['lib/asmcrypto.js/asmcrypto.min.js', 'dist/web-crypto-src.min.js'],
-          'dist/web-crypto-promise.js': ['bower_components/es6-promise/promise.js', 'dist/web-crypto.js'],
-          'dist/web-crypto-promise.min.js': ['bower_components/es6-promise/promise.min.js', 'dist/web-crypto.min.js']
-        }
+        dest: 'dist/web-crypto.js'
       }
     },
     uglify: {
@@ -67,31 +50,17 @@ module.exports = function(grunt) {
           sourceMap: true
         },     
         files: {
-          'dist/web-crypto-src.min.js': ['dist/web-crypto-src.js']
-        }
-      },
-      asmcrypto: {
-        options: {
-          mangle: {},
-          compress: {},
-          sourceMap: true,
-          sourceMapIn: 'lib/asmcrypto.js/asmcrypto.js.map',
-          sourceMapIncludeSources: true,
-          screwIE8: true,
-          banner: "/*! asmCrypto<%= asmPkg.version && ' v'+asmPkg.version %>, (c) 2013 <%= asmPkg.author.name %>, opensource.org/licenses/<%= asmPkg.license %> */"
-        },
-        files: {
-          'lib/asmcrypto.js/asmcrypto.min.js': 'lib/asmcrypto.js/asmcrypto.js'
+          'dist/web-crypto.min.js': ['dist/web-crypto.js']
         }
       }
     },
     subgrunt: {
       options: {
-        npmInstall: false
+        npmInstall: true
       },
       build_asm: {
         projects: {
-          'lib/asmcrypto.js': ['sources', '--with=common, utils, origin, exports, globals, aes, aes-ecb, aes-cbc, aes-cfb, aes-ofb, aes-ctr, aes-ccm, aes-gcm, aes-exports, aes-ecb-exports, aes-cbc-exports, aes-cfb-exports, aes-ofb-exports, aes-ctr-exports, aes-ccm-exports, aes-gcm-exports, hash, sha1, sha1-exports, sha256, sha256-exports, sha512, sha512-exports, hmac, hmac-sha1, hmac-sha256, hmac-sha512, hmac-sha1-exports, hmac-sha256-exports, hmac-sha512-exports, pbkdf2, pbkdf2-hmac-sha1, pbkdf2-hmac-sha256, pbkdf2-hmac-sha512, pbkdf2-hmac-sha1-exports, pbkdf2-hmac-sha256-exports, pbkdf2-hmac-sha512-exports, rng, rng-exports, bn, bn-exports, rsa, rsa-raw, rsa-pkcs1, rsa-keygen-exports, rsa-raw-exports, rsa-oaep-sha1-exports, rsa-oaep-sha256-exports, rsa-oaep-sha512-exports, rsa-pss-sha1-exports, rsa-pss-sha256-exports, rsa-pss-sha512-exports', 'concat']
+          'repos/asmcrypto.js': ['default', '--with=common, utils, origin, exports, globals, aes, aes-ecb, aes-cbc, aes-cfb, aes-ofb, aes-ctr, aes-ccm, aes-gcm, aes-exports, aes-ecb-exports, aes-cbc-exports, aes-cfb-exports, aes-ofb-exports, aes-ctr-exports, aes-ccm-exports, aes-gcm-exports, hash, sha1, sha1-exports, sha256, sha256-exports, sha512, sha512-exports, hmac, hmac-sha1, hmac-sha256, hmac-sha512, hmac-sha1-exports, hmac-sha256-exports, hmac-sha512-exports, pbkdf2, pbkdf2-hmac-sha1, pbkdf2-hmac-sha256, pbkdf2-hmac-sha512, pbkdf2-hmac-sha1-exports, pbkdf2-hmac-sha256-exports, pbkdf2-hmac-sha512-exports, rng, rng-exports, bn, bn-exports, rsa, rsa-raw, rsa-pkcs1, rsa-keygen-exports, rsa-raw-exports, rsa-oaep-sha1-exports, rsa-oaep-sha256-exports, rsa-oaep-sha512-exports, rsa-pss-sha1-exports, rsa-pss-sha256-exports, rsa-pss-sha512-exports']
         }
       }
     },
@@ -113,35 +82,23 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-git');
-  grunt.loadNpmTasks('grunt-auto-install');
   grunt.loadNpmTasks('grunt-subgrunt');
   grunt.loadNpmTasks('grunt-contrib-clean');
   
-  grunt.registerTask('install', 'Install dependencies', function() {
-    var asmExists = grunt.file.exists('lib/asmcrypto.js');
+  grunt.registerTask('buildAsmCrypto', 'Install and build asmCrypto', function() {
+    
+    // Clone asmCrypto repository
+    var asmExists = grunt.file.exists('repos/asmcrypto.js');
     if(!asmExists) {
       grunt.task.run('gitclone:asmcrypto');
     }
-    grunt.task.run('auto_install');
-  });
-  
-  grunt.registerTask('default', 'Build files', function() {
     
-    // Build asmCrypto
-    grunt.config('asmPkg', grunt.file.readJSON('lib/asmcrypto.js/package.json'));
-    grunt.task.run(['subgrunt:build_asm', 'uglify:asmcrypto']);
-    
-    // Build sources
-    grunt.task.run(['concat:source', 'uglify:source']);
-    
-    // Concat sources and asmCrypto
-    grunt.task.run('concat:default');
-    
-    // Delete web-crypto-src.* files
-    grunt.task.run('clean:src');
+    // Build asmCrypto files
+    grunt.task.run('subgrunt:build_asm');
     
   });
   
+  grunt.registerTask('default', ['concat:source', 'uglify:source']);
   grunt.registerTask('doc', ['jsdoc:source']);
 
 };
