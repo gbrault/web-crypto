a5bae738cdce8d8adedc75ee348af05a44e2c069
Samuel Samtleben <samuel.samtleben@ausdatteln.de>
2016-01-11 15:22:18 +0100

    Added functions stringToBytes and bytesToString to util exports

--- dist/web-crypto.js
+++ dist/web-crypto.js
@@ -2693,6 +2693,8 @@ exports.util.bytesToBase64URL = bytesToBase64URL;
 exports.util.base64URLToBytes = base64URLToBytes;
 exports.util.bytesToHex = bytesToHex;
 exports.util.hexToBytes = hexToBytes;
+exports.util.stringToBytes = stringToBytes;
+exports.util.bytesToString = bytesToString;
 /**
  * Creates inheritance.
  * 
